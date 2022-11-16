f69faf4d36b3678929aa652bffe220515940b780
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-11-16 08:39:55 -0800

    strings.c

--- path.c
+++ path.c
@@ -1,132 +1,84 @@
 #include "shell.h"
-
 /**
- * check_path - checks if command is present in PATH.
- * @argv: arguments
- * @env: environment
- *
- * Return: if the command is found, the function will return 1. Else, the
- * function will return 0.
+ * pathvalue - value of environ.
+ * @env: an array of string to environ
+ * Return: token or NULL.
  */
-int check_path(char *argv[], char *env[])
+char *pathvalue(char **env)
 {
-	char *path = _path(env), *check = NULL, **routes = NULL;
-	struct stat buf;
+	char *token, *temp;
 	int i = 0;
 
-	if (path != NULL && *path)
+	while (env[i])
 	{
-		if (check_cwd(argv))
-			return (1);
-		routes = _tokenize(path, ": ");
-		check = expand(check, 0, (sizeof(*check) * _strlen(routes[i]))
-				+ (sizeof(*check) * _strlen(argv[0]) + 2));
-		while (routes[i])
+		temp = env[i];
+		token = strtok(temp, "=");
+		if (_strcmp(token, "PATH") == 0)
 		{
-			_strcpy(check, routes[i]);
-			_strcat(check, "/");
-			_strcat(check, argv[0]);
-			if (stat(check, &buf) == 0)
-			{
-				free(argv[0]);
-				argv[0] = check;
-				free_double((void **) routes, ec((void **) routes));
-				return (1);
-			}
-			_memset(check, 0, _strlen(check));
-			i++;
-			if (routes[i])
-				check = expand(check, sizeof(check), (sizeof(*check) * _strlen(routes[i]))
-						+ (sizeof(*check) * _strlen(argv[0]) + 2));
+			token = strtok(NULL, "=");
+			return (token);
 		}
-		free_double((void **) routes, ec((void **) routes));
-		free(check);
+		i++;
+	}
+	return (NULL);
+}
+/**
+ * _strstr -  a function that locates a substring..
+ * @haystack: string character value.
+ * @needle: substring character value.
+ * Return: Returns a pointer to substring.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j = 0;
+
+	if (needle[j] == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+		for (j = 0; needle[j] != '\0' && haystack[i + j]
+		&& needle[j] == haystack[i + j]; j++)
+			;
+		if (needle[j] == 0)
+			return (haystack + i);
+		}
 	}
 	return (0);
 }
 /**
- * _path - checks for the environment variable PATH.
- * @env: environment
- *
- * Return: pointer to the environment variable PATH, five steps forward.
+ * pathch - tokenize path and check if the buffer exists.
+ * @token: the string tokenizedddd the buffer in startsh file.
+ * @env: array of string to environ.
+ * Return: the buffer.
  */
-char *_path(char **env)
+char *pathch(char *token, char **env)
 {
-	int i = 0, j = 0;
-	char *path = "PATH=";
+	static char buffer[1024] = {0};
+	char *path, *a, *b, *tok;
+	struct stat st;
 
-	while (env[i])
+	path = pathvalue(env);
+	if (!path)
+		path = "";
+	a = _strstr(path, "::");
+	b = _strstr(path, ":/bin");
+	if (path[0] == ':' || (a && (a < b)))
 	{
-		j = 0;
-		while (j < 5)
-		{
-			if (env[i][j] != path[j])
-				break;
-			j++;
-		}
-		if (j == 5)
-			break;
-		i++;
+		if (stat(token, &st) == 0)
+		return (token);
 	}
-	return (&env[i][j]);
-}
-
-/**
- * cwd - current working diretory
- *
- * Return: char * containing the absolute current working directory
- */
-char *cwd()
-{
-	char *cwd = NULL;
-
-	return (getcwd(cwd, 0));
-}
-
-/**
- * absolute_path - get absolute path using the current working directory.
- * @path: path to the executable
- *
- * Return: generated absolute path
- */
-char *absolute_path(char *path)
-{
-	char *wd = cwd();
-	char *ab_path = malloc((sizeof(*ab_path) * _strlen(wd)) +
-			(sizeof(*ab_path) * _strlen(path)) + 2);
-
-	_strcpy(ab_path, wd);
-	_strcat(ab_path, "/");
-	_strcat(ab_path, path);
-
-	free(wd);
-	return (ab_path);
-}
-
-/**
- * check_cwd - checks if path from absolute current working directory exits.
- * @argv: arguments
- *
- * Return: if path exists, the function will return 1, else, the function
- * will return 0.
- */
-int check_cwd(char *argv[])
-{
-	char *check = NULL;
-	struct stat buf;
-
-	if (argv[0][0] == '.' && argv[0][1] == '/')
+	tok = strtok(path, ":");
+	while (tok)
 	{
-		check =	absolute_path(&argv[0][2]);
-		if (stat(check, &buf) == 0)
-		{
-			free(argv[0]);
-			argv[0] = check;
-			return (1);
-		}
-		else
-			return (0);
+		_strcat(buffer, tok);
+		_strcat(buffer, "/");
+		_strcat(buffer, token);
+		if (stat(buffer, &st) == 0)
+			return (buffer);
+		tok = strtok(NULL, ":");
+		_memset(buffer, 0, 1024);
 	}
-	else
-		return (0);
+	return (token);
 }
--- shell.h
+++ shell.h
@@ -1,63 +1,33 @@
-#ifndef _shell_
-#define _shell_
-
-#include <unistd.h>
+#ifndef SHELL_H
+#define SHELL_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <string.h>
 
-#define GREEN "\x1B[32m"
-#define YELLOW "\x1B[33m"
-#define RESET "\x1B[0m"
-
-/**
- * struct builtin - shell builtin
- * @name: builtin name
- * @f: function associated with the builtin
- *
- */
-typedef struct builtin
-{
-	char *name;
-	void (*f)(char *env[]);
-} builtin_t;
-
-void _get_input(char *env[]);
-char *_next(void);
-void show_prompt(void);
-void _validate(char *buffer, char *env[]);
-void _execute(char *argv[], char *env[]);
-
-int tc(char *str, char *delim);
-char **_tokenize(char *buffer, char *delimiter);
-char *_strtok(char *str, char *delim);
-char **_strfield(char *str, char delim);
-
-int _putstr(char *str);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *a, char *b);
-int _strlen(char *s);
-int charcmp(char c, char *cmp);
+extern char **environ;
+void _puts(char *str);
+int _putchar(char c);
+char *_strdup(char *str);
+char **handletok(char *buffer);
+char *pathvalue(char **env);
+char *pathch(char *token, char **env);
+char *_strstr(char *haystack, char *needle);
 int _strcmp(char *s1, char *s2);
-
-char *_path(char **env);
-int check_path(char *argv[], char *env[]);
-char *cwd();
-char *absolute_path(char *path);
-int check_cwd(char *argv[]);
-
-int ec(void **ptr);
-
-void *_memset(char *s, int c, size_t n);
-void free_double(void **ptr, int i);
-void *expand(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_memcpy(char *dest, char *src, unsigned int n);
-
-void (*check_for_builtins(char **argv, char *env[]))(char *env[]);
-void n_exit(char *env[]);
-void _env(char *env[]);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strcat(char *dest, char *src);
+int _strlen(char *str);
+char *_memset(char *s, char b, unsigned int n);
+void builtin(char **tokenize, char **env, char **buff, int status);
+char *readline(void);
+void sfree(char *str);
+void nfree(char **str);
+void env_builtin(char **env);
+void _free(char **tokenize, char *buff);
+void __free(char **tokenize, char *buff);
+int __exit(int status);
 
 #endif
